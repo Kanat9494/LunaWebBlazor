761a54ea56002b9a4eab08a2e5ed85d4861223aa
Kanat9494 <kkanat94@gmail.com>
2024-01-06 23:34:07 +0600

    добавил модель

---
+++ LunaWebBlazor/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace LunaWebBlazor.Models;
+
+public class Car
+{
+    public int CarId { get; set; }
+    public string Color { get; set; }
+    public string Title { get; set; }
+    public int YearOfIssue { get; set; }
+    public string SubTitle { get; set; }
+    public double Volume { get; set; }
+    public int Mileage { get; set; }
+    public string Location { get; set; }
+    public List<string> Images { get; set; }
+}
